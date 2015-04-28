b44ce4cd5d763cdd789c163499052085481da5d7
Jakub Zapletal <zapletal.jakub@gmail.com>
2015-04-28 01:02:19 +0100

    Initialize app skeleton

---
+++ src/Application.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Training;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class Application extends BaseApplication
+{
+    /** @var ContainerBuilder */
+    private $container;
+
+    public function __construct()
+    {
+        $this->container = new ContainerBuilder();
+
+        $loader = new YamlFileLoader($this->container, new FileLocator(__DIR__ . '/../config'));
+        $loader->load('services.yml');
+
+        parent::__construct();
+    }
+}
